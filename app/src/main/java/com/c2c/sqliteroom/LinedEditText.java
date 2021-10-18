7e69c40958e5c2e39670c9ab0110282cd24cb71d
Sahil Singh <singhsittu430@gmail.com>
2021-10-18 19:43:12 +0530

    :lipstick: Added: Line Styles to Note Content

--- app/src/main/java/com/c2c/sqliteroom/LinedEditText.java
+++ app/src/main/java/com/c2c/sqliteroom/LinedEditText.java
@@ -18,6 +18,12 @@ public class LinedEditText extends AppCompatEditText {
 
     public LinedEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        mRect = new Rect();
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(2);
+        mPaint.setColor(0xFFFFD966);
     }
 
     @Override
@@ -31,8 +37,9 @@ public class LinedEditText extends AppCompatEditText {
 
         int baseline = getLineBounds(0, rect);
 
-        for(int i=0;i<numOfLines; i++){
-             
+        for(int i=0; i<numOfLines; i++){
+             canvas.drawLine(rect.left, baseline+1, rect.right, baseline+1, paint);
+             baseline += lineHeight;
         }
 
         super.onDraw(canvas);

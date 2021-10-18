fe264c7a2d88c60a486ed07e667cd4045101374a
Sahil Singh <singhsittu430@gmail.com>
2021-10-18 20:30:12 +0530

    :wrench: Setting: Note Properties

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,9 +7,3 @@ P
 Kapp/src/main/java/com/c2c/sqliteroom/util/VerticalSpacingItemDecorator.java,6/f/6f63e2de8d3cac9f8021ca6757d1acb5859d5579
 w
 Gapp/src/main/java/com/c2c/sqliteroom/adapters/NotesRecyclerAdapter.java,5/f/5f1a15858cde84a5c86ac9920b20969993e1853f
-k
-;app/src/main/java/com/c2c/sqliteroom/NotesListActivity.java,1/0/1030ecae69aa2f6598fca28d60785ae0ffb48e6c
-e
-5app/src/main/java/com/c2c/sqliteroom/models/Note.java,b/f/bff2470f42d19a7270c435e653e7bda5f4890e5f
-n
->app/src/main/java/com/c2c/sqliteroom/NotesContentActivity.java,a/d/adbad9e34b890cc571a436bedb594ad0b732ab8b
--- app/src/main/java/com/c2c/sqliteroom/NotesContentActivity.java
+++ app/src/main/java/com/c2c/sqliteroom/NotesContentActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.c2c.sqliteroom.models.Note;
 
@@ -11,14 +13,54 @@ public class NotesContentActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesContentActivity";
 
+    //ui components
+    private LinedEditText mLinedEditText;
+    private TextView mViewTitle;
+    private EditText mEditTitle;
+
+    //vars
+    private boolean mIsNewNote;
+    private Note mCurrentNote;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_content);
 
-        if(getIntent().hasExtra("Selected_note")) {
-            Note note = getIntent().getParcelableExtra("Selected_note");
-            Log.d(TAG, "onCreate: Note Content" + note.toString());
+        mLinedEditText = findViewById(R.id.notes_content_text);
+        mViewTitle = findViewById(R.id.toolbar_note_view_title);
+        mEditTitle = findViewById(R.id.toolbar_note_edit_title);
+
+        if(getIncomingIntent()){
+            //new note -> {Edit Mode}
+            setNewNoteProperties();
         }
+        else{
+            //old note -> {View Mode}
+            setNoteProperties();
+        }
+    }
+
+    public boolean getIncomingIntent(){
+        if(getIntent().hasExtra("selected_note")){
+            mCurrentNote = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "onCreate: Note Content" + mCurrentNote.toString());
+            mIsNewNote = false;
+            return false;
+        }
+        mIsNewNote = true;
+        return true;
+    }
+
+    private void setNoteProperties(){
+        mViewTitle.setText(mCurrentNote.getTitle());
+        mEditTitle.setText(mCurrentNote.getTitle());
+        mLinedEditText.setText(mCurrentNote.getContent());
+    }
+
+    private void setNewNoteProperties(){
+        mViewTitle.setText("Note's Title");
+        mEditTitle.setText("Note's Title");
     }
 }
